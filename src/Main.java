e7a3634b98671d5ec802a0d87d54f74e220d27c8
Michi <mblaimsc@htl-steyr.ac.at>
2020-11-11 12:12:42 +0100

    Merge branch 'master' of D:\Projekt\HTLSteyr\3 AHITN\ITP2\3AHITN_Einfuehrung_Git with conflicts.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo Fabian");
         System.out.println("Hallo Michael");
-        System.out.println("Selbe Zeile");
     }
 }
